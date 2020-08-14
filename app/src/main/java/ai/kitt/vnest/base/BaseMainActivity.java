a6f9cd9cf31bd48f5c02f8030629d742f0960ab1
tuan.do <tuan.do@roxwin.com>
2020-08-14 10:24:48 +0700

    - divide view/controller main activity

--- app/src/main/java/ai/kitt/vnest/base/BaseMainActivity.java
+++ app/src/main/java/ai/kitt/vnest/base/BaseMainActivity.java
@@ -2,8 +2,11 @@ package ai.kitt.vnest.base;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.app.SearchManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -16,28 +19,44 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.speech.SpeechRecognizer;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.kwabenaberko.openweathermaplib.implementation.OpenWeatherMapHelper;
+import com.kwabenaberko.openweathermaplib.implementation.callbacks.CurrentWeatherCallback;
+import com.kwabenaberko.openweathermaplib.models.currentweather.CurrentWeather;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import ai.api.AIServiceException;
 import ai.api.android.AIConfiguration;
 import ai.api.android.AIService;
+import ai.api.model.AIContext;
 import ai.api.model.AIOutputContext;
+import ai.api.model.AIRequest;
 import ai.api.model.AIResponse;
+import ai.kitt.snowboy.service.TriggerBroadCast;
 import ai.kitt.vnest.App;
 import ai.kitt.vnest.basedata.api.model.ActiveCode;
 import ai.kitt.vnest.basedata.api.model.ActiveResponse;
@@ -45,6 +64,7 @@ import ai.kitt.vnest.basedata.api.model.CarResponse;
 import ai.kitt.vnest.basedata.database.sharepreference.VnestSharePreference;
 import ai.kitt.vnest.basedata.entity.Audio;
 import ai.kitt.vnest.basedata.entity.Message;
+import ai.kitt.vnest.basedata.entity.MyAIContext;
 import ai.kitt.vnest.basedata.entity.Poi;
 import ai.kitt.vnest.basedata.entity.Youtube;
 import ai.kitt.vnest.databinding.ActivityMainBinding;
@@ -56,12 +76,14 @@ import ai.kitt.vnest.feature.activitymain.ViewModelFactory;
 import ai.kitt.vnest.feature.activitymain.adapters.ItemNavigationAdapter;
 import ai.kitt.vnest.feature.screenhome.FragmentHome;
 import ai.kitt.vnest.feature.screenspeech.FragmentResult;
+import ai.kitt.vnest.speechmanager.speechonline.OnResultReady;
 import ai.kitt.vnest.speechmanager.speechonline.SpeechRecognizerManager;
 import ai.kitt.vnest.speechmanager.texttospeech.TextToSpeechManager;
 import ai.kitt.vnest.util.AppUtil;
 import ai.kitt.vnest.util.ConfirmDialog;
 import ai.kitt.vnest.util.DialogActiveControl;
 import ai.kitt.vnest.util.DialogUtils;
+import kun.ktupdatelibrary.DownLoadBroadCast;
 import kun.ktupdatelibrary.UpdateChecker;
 
 public abstract class BaseMainActivity extends AppCompatActivity implements LocationListener, TextToSpeechManager.TextToSpeechListener, ViewModel.OnOpenVtvListener,
@@ -164,7 +186,14 @@ public abstract class BaseMainActivity extends AppCompatActivity implements Loca
     protected OpenWeatherMapHelper weather;
     protected LocationManager locationManager;
     protected AIService aiService;
-
+    protected boolean notchangesessionid = false;
+    protected String currentSessionId;
+    protected long processTime = 0;
+    public static boolean isExcecuteText = false;
+    protected Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+    private Boolean isStartRecognizer;
+    public DownLoadBroadCast downLoadBroadCast;
+    private TriggerBroadCast triggerBroadCast;
 
 
     public SpeechRecognizerManager getSpeechRecognizerManager() {
@@ -198,7 +227,24 @@ public abstract class BaseMainActivity extends AppCompatActivity implements Loca
     }
 
 
-    protected abstract void initIfPermissionGranted();
+    protected void initIfPermissionGranted(){
+        initView();
+        initAction();
+    }
+    protected abstract void  initView();
+    private void initAction() {
+        sendCarInfo();
+        initSpeechRecognizerManager();
+        viewModel.getLiveDataUpdateResponse().observe(this, this::updateApp);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        onLocationChanged(location);
+        initTextToSpeech();
+    }
 
     protected void initAIMasterService() {
         final AIConfiguration config = new AIConfiguration(AI_CONFIG_ACCESS_TOKEN,
@@ -275,11 +321,229 @@ public abstract class BaseMainActivity extends AppCompatActivity implements Loca
         textToSpeech = TextToSpeechManager.getInstance(this);
         textToSpeech.setTextToSpeechListener(this);
     }
+    protected void initSpeechRecognizerManager() {
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this, ComponentName.unflattenFromString("com.google.android.googlequicksearchbox/com.google.android.voicesearch.serviceapi.GoogleRecognitionService"));
+        speechRecognizerManager = SpeechRecognizerManager.getInstance(this, new OnResultReady() {
+            @Override
+            public void onResults(@NotNull ArrayList<String> results) {
+                if (isExcecuteText) {
+                    return;
+                }
+                speechRecognizerManager.muteVolume(false);
+                finishRecognition();
+                speechRecognizerManager.stopListening();
+                String text = results.get(0);
+                isExcecuteText = true;
+                sendMessage(text, true);
+                processing_text(text, false);
+                Log.d(LOG_TAG, "onResults: " + text);
+            }
 
+            @Override
+            public void onStreamResult(@NotNull ArrayList<String> partialResults) {
+
+            }
+        }, speechRecognizer, () -> viewModel.getLiveDataRebindRecognitionsView().postValue(true));
+    }
+    public void finishRecognition() {
+        isStartRecognizer = false;
+        if (speechRecognizerManager != null) {
+            speechRecognizerManager.stopListening();
+        }
+    }
+    public void processing_text(final String text, Boolean resetContext) {
+        Log.d(LOG_TAG, "================= processing_text: " + text);
+        Thread thread = new Thread(() -> {
+            try {
+                AIRequest aiRequest = new AIRequest(text);
+                if (!notchangesessionid || currentSessionId == null) {
+                    currentSessionId = deviceId + "#" + latitude + "-" + longitude;
+                }
+                aiRequest.setSessionId(currentSessionId);
+                long currentProcessTime = System.currentTimeMillis();
+                if (resetContext || calculateResetContext(processTime, currentProcessTime) || contexts == null) {
+                    Log.e(LOG_TAG, "============Reset context=============");
+                    contexts = null;
+                    aiRequest.setResetContexts(true);
+                    aiRequest.setContexts(null);
+                }
+                processTime = currentProcessTime;
+                if (contexts != null) {
+                    List<AIContext> rqContexts = new ArrayList<>();
+                    for (AIOutputContext oc : contexts) {
+                        rqContexts.add(new MyAIContext(oc));
+                    }
+                    aiRequest.setContexts(rqContexts);
+                }
+
+                Log.d(LOG_TAG, "===== aiRequest:" + gson.toJson(aiRequest));
+
+                try {
+                    AIResponse aiRes = aiService.textRequest(aiRequest);
+                    Log.d(LOG_TAG, gson.toJson(aiRes));
+                    String action = aiRes.getResult().getAction();
+                    Log.e(LOG_TAG, "===== action:" + action);
+                    contexts = aiRes.getResult().getContexts();
+                    String code = null;
+                    try {
+                        notchangesessionid = aiRes.getResult().getFulfillment().getData().get(NOT_CHANGE_SESSION_ID).getAsBoolean();
+                        code = aiRes.getResult().getFulfillment().getData().get("code").toString().replace("\"", "");
+                        Log.d(LOG_TAG, "===== " + NOT_CHANGE_SESSION_ID + ": " + notchangesessionid);
+                    } catch (Exception e) {
+                        Log.e(LOG_TAG, e.getMessage(), e);
+                    }
+                    switch (action) {
+                        case OPEN_BANK_PLACE_UN_KNOW_SPEECH:
+                        case OPEN_DRINK_PLACE_UN_KNOW_SPEECH:
+                        case OPEN_EAT_PLACE_UN_KNOW_SPEECH:
+                        case OPEN_MAP_TO_SPEECH:
+                        case OPEN_BANK_PLACE_WHATEVER:
+                            searchPlaceUnknown(text, aiRes);
+                            break;
+                        case OPEN_BANK_PLACE:
+                        case OPEN_DRINK_PLACE:
+                        case OPEN_DRINK_PLACE_UN_KNOW:
+                        case OPEN_EAT_PLACE:
+                        case OPEN_EAT_PLACE_UN_KNOW:
+                        case OPEN_EAT_PLACE_WHATEVER:
+                        case OPEN_BANK_PLACE_UN_KNOW:
+                        case OPEN_PLACE:
+                        case OPEN_MAP_TO:
+                            viewModel.searchPlaces(action, aiRes, this);
+                            break;
+                        case ACTION_OPEN_VTV:
+                            viewModel.openVtv(aiRes, code, this);
+                            break;
+                        case INPUT_UN_KNOW:
+                            searchInputUnknown(aiRes);
+                            break;
+                        case OPEN_MP3:
+                            viewModel.searchMp3(code, aiRes, this);
+                            break;
+                        case OPEN_YOU_TUBE:
+                            viewModel.searchYoutube(aiRes, code, this);
+                            break;
+                        case ACTION_CALL_FROM_CONTACT:
+                            viewModel.callTo(this, aiRes, code, this);
+                            break;
+                        default:
+                            if (text.toLowerCase().contains(KEY_WEATHER)) {
+                                weather();
+                            } else if (text.toLowerCase().contains(KEY_SEARCH)) {
+                                viewModel.search(text, this);
+                            } else {
+                                sendMessage(NO_DATA_FOUND, false);
+                                speak(NO_DATA_FOUND, false);
+                            }
+                    }
+
+
+                } catch (AIServiceException e) {
+                    Log.e(LOG_TAG, e.getMessage(), e);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                isExcecuteText = false;
+            }
+        });
+        thread.start();
+    }
     protected void resetContext() {
         contexts = null;
     }
+    protected void weather() {
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("please waiting....");
+        progressDialog.show();
 
+        weather.getCurrentWeatherByGeoCoordinates(latitude, longitude, new CurrentWeatherCallback() {
+            @Override
+            public void onSuccess(CurrentWeather currentWeather) {
+
+                progressDialog.dismiss();
+
+                Date timeSunrise = new Date(currentWeather.getSys().getSunrise() * 1000);
+                Date timeSunset = new Date(currentWeather.getSys().getSunset() * 1000);
+
+                @SuppressLint("SimpleDateFormat") DateFormat dateFormat = new SimpleDateFormat("hh:mm a");
+
+                String sunrise = dateFormat.format(timeSunrise);
+                String sunset = dateFormat.format(timeSunset);
+
+                Log.v("Weather", "Coordinates: " + currentWeather.getCoord().getLat() + ", " + currentWeather.getCoord().getLon() + "\n"
+                        + "Weather Description: " + currentWeather.getWeather().get(0).getDescription() + "\n"
+                        + "Temperature: " + currentWeather.getMain().getTempMax() + "\n"
+                        + "Wind Speed: " + currentWeather.getWind().getSpeed() + "\n"
+                        + "City, Country: " + currentWeather.getName() + ", " + currentWeather.getSys().getCountry() + "\n"
+                        + "Time sunrise: " + sunrise + "\n"
+                        + "Time sunset: " + sunset + "\n"
+                );
+
+                String location = currentWeather.getName() + ", " + currentWeather.getSys().getCountry();
+                String description = currentWeather.getWeather().get(0).getDescription();
+                String wind = String.valueOf(currentWeather.getWind().getSpeed());
+                String tempMax = String.valueOf(currentWeather.getMain().getTempMax());
+                String humidity = String.valueOf(currentWeather.getMain().getHumidity());
+
+                show_weather(location, description, tempMax, wind, humidity, sunrise, sunset);
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                progressDialog.dismiss();
+                sendMessage("Lỗi, Không thế tìm kiếm thời tiết tại vị trí của bạn.", false);
+                Log.d("Weather", throwable.getMessage(), throwable);
+            }
+        });
+
+    }
+
+
+    /**
+     * Show dialog weather in location
+     *
+     * @param location
+     * @param description
+     * @param tempMax
+     * @param wind
+     * @param humidity
+     * @param sunrise
+     * @param sunset
+     */
+    protected void show_weather(String location, String description, String tempMax, String
+            wind, String humidity, String sunrise, String sunset) {
+
+        String w = "Thời tiết " + location + " : " + description + " " + tempMax + "\u2103";
+
+        sendMessage(w, false);
+
+        String textSpeech = "Thời tiết " + location + " : " + description + " " + tempMax + "độ xê";
+        textToSpeech.speak(textSpeech, false);
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.weather);
+
+        dialog.setContentView(R.layout.weather);
+
+        TextView tvLocation, tvDescription, tvTempMax, tvWind, tvHumidity, tvSunrise, tvSunset;
+        tvLocation = dialog.findViewById(R.id.tvLocation);
+        tvDescription = dialog.findViewById(R.id.tvDescription);
+        tvTempMax = dialog.findViewById(R.id.tvTempMax);
+        tvWind = dialog.findViewById(R.id.tvWind);
+        tvHumidity = dialog.findViewById(R.id.tvHumidity);
+        tvSunrise = dialog.findViewById(R.id.tvSunrise);
+        tvSunset = dialog.findViewById(R.id.tvSunset);
+
+        tvLocation.setText(location);
+        tvDescription.setText(description);
+        tvTempMax.setText(tempMax);
+        tvWind.setText(wind);
+        tvHumidity.setText(humidity);
+        tvSunrise.setText(sunrise);
+        tvSunset.setText(sunset);
+
+        dialog.show();
+    }
     public void sendMessage(String text, boolean isUser) {
         viewModel.getLiveDataProcessText().postValue(new Message(text, isUser, Calendar.getInstance().getTimeInMillis()));
     }
@@ -608,5 +872,110 @@ public abstract class BaseMainActivity extends AppCompatActivity implements Loca
         intent.putExtra(SearchManager.QUERY, keySearch);
         startActivity(intent);
         resetContext();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "stop TRIGGER");
+        if (downLoadBroadCast == null) {
+            ProgressDialog waitingDialog = DialogUtils.showProgressDialog(this);
+            downLoadBroadCast = DownLoadBroadCast.initBroadCast(this, new DownLoadBroadCast.OnReceive() {
+                @Override
+                public void onReceiveProgress(int progress) {
+                    waitingDialog.dismiss();
+                    initProgressDialog();
+                    downloadDialog.setMessage("Downloading... ");
+                    downloadDialog.setProgress(progress);
+                    downloadDialog.show();
+                }
+
+                @Override
+                public void onFinish() {
+                }
+
+                @Override
+                public void onWaiting() {
+                    waitingDialog.show();
+                }
+
+                @Override
+                public void onRetry() {
+                    updateApp(viewModel.carInfoResponse);
+                }
+            });
+        }
+
+        if (triggerBroadCast == null) {
+            triggerBroadCast = TriggerBroadCast.initBroadCast(this, new TriggerBroadCast.OnHandleTrigger() {
+                @Override
+                public void onActionTurnOn() {
+                    if (App.isActivated) {
+                        startResultFragment();
+                        viewModel.getLiveDataOpenVTV().postValue("-1");
+                        viewModel.getLiveDataStartRecord().postValue(true);
+                    }
+                }
+
+                @Override
+                public void onActionTurnOff() {
+
+                }
+
+                @Override
+                public void onActionStartApp() {
+
+                }
+            }, MainActivity.class);
+        }
+        if (checkPermission()) {
+            if (AppUtil.checkInternetConnection(this) && App.isActivated) {
+                startResultFragment();
+                viewModel.getLiveDataStartRecord().postValue(true);
+            } else {
+                startHomeFragment();
+            }
+        }
+    }
+
+    /**
+     * Stop the recognizer.
+     * Since cancel() does trigger an onResult() call,
+     * we cancel the recognizer rather then stopping it.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "start TRIGGER");
+        resetContext();
+        if (checkPermission()) {
+            finishRecognition();
+            if (speechRecognizer != null) {
+                speechRecognizer.stopListening();
+            }
+
+        }
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        Log.e(LOG_TAG, "onDestroy");
+        super.onDestroy();
+        if (speechRecognizer != null) {
+            speechRecognizer.destroy();
+        }
+        //Start service
+        if (downLoadBroadCast != null) {
+            unregisterReceiver(downLoadBroadCast);
+        }
+        TriggerBroadCast.unregisterBroadCast(this, triggerBroadCast);
+        if (downloadDialog != null) {
+            downloadDialog.dismiss();
+        }
+        if (dialogActiveControl != null) {
+            dialogActiveControl.dismiss();
+        }
     }
 }
